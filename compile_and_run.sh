7b58998c3176dc215b26db82829fd3e304bb706e
Pablo D. Roca <pablodroca@gmail.com>
2016-01-24 22:47:56 -0300

    Removed classpath modifications in Linux bash due Java8 usage.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,16 +1,10 @@
 #! /bin/bash
-#if [ -z "$JAVA_HOME" ] && [[ "$CLASSPATH" ~= "jfxrt.jar" ]]; then
 
 if [[ ! "$(javac -version 2>&1)" =~ "1.8" ]]; then
-  if [ -z "$JAVA_HOME" ] && [[ ! "$CLASSPATH" =~ "jfxrt.jar" ]]; then
-    echo "Please define either a valid JAVA_HOME or a CLASSPATH with jfxrt.jar to ensure the JavaFX components can work at JDK <1.8"
+  echo "MultiMarioBros works with JavaFX libraries included in Java 8. Previous versions of Java are not supported but could work by including the proper jfxrt.jar into the CLASSPATH."
     exit -1
-  fi
-  if [ -z "$JAVA_HOME" ]; then
-    classpath=$CLASSPATH
-  else
-    classpath="$JAVA_HOME/lib/jre/lib/jfxrt.jar"
-  fi
+else
+  classpath=$CLASSPATH
 fi
 
 mkdir -p bin
